aea9f99ec33c0291b2df1507ca9c6a978a51cb39
Joshtdale <joshuadale65@gmail.com>
2022-09-08 21:10:02 +0000

    madlib working

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,6 +1,15 @@
 // console.log('script working')
 
 const customName = document.getElementById('customname');
+const customAdverb = document.getElementById('customAdverb')
+const customAdjective = document.getElementById('customAdjective')
+const customPluralNoun = document.getElementById('customPluralNoun')
+const customPluralNoun2 = document.getElementById('customPluralNoun2')
+const customEmotion = document.getElementById('customEmotion')
+const customVerb = document.getElementById('customVerb')
+const customAdjective2 = document.getElementById('customAdjective2')
+
+
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
@@ -9,9 +18,9 @@ const story = document.querySelector('.story');
         return array[random];
     }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const storyText = "There is a house on my :insertx: that is :adverb: haunted. It's the old :inserty: place that's been :adjective: for hundreds of years. I can tell the house is :adjective: because there are :pluralNoun: and :adjective: :pluralNoun2: outside, and it smells like old :food:. I heard that a kid named Bob went inside and never came back out. My friends and I are :emotion: to :verb: past the house because it's so :adjective2:";
+const insertX = ['street', 'block'];
+const insertY = ['Frankenstein', 'Dracula', 'Awesome Inc'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
@@ -25,13 +34,53 @@ randomize.addEventListener('click', result);
         var newStory = newStory.replace(':insertx:', xItem);
         var newStory = newStory.replace(':inserty:', yItem);
         var newStory = newStory.replace(':insertz:', zItem);
-        var newStory = newStory.replace(':insertx:', xItem);
+        // var newStory = newStory.replace(':insertx:', xItem);
 
         if(customName.value !== '') {
             const name = customName.value;
             newStory = newStory.replace('Bob', name);
         }
 
+        if(customAdverb.value !== '') {
+            const adverb = customAdverb.value;
+            newStory = newStory.replace(':adverb:', adverb);
+        }
+
+        //example of replaceAll replacing more than one substring
+        if(customAdjective.value !== '') {
+            const adjective = customAdjective.value;
+            newStory = newStory.replaceAll(':adjective:', adjective);
+        }
+
+        if(customPluralNoun.value !== '') {
+            const pluralNoun = customPluralNoun.value;
+            newStory = newStory.replace(':pluralNoun:', pluralNoun);
+        }
+
+        if(customPluralNoun2.value !== '') {
+            const pluralNoun2 = customPluralNoun2.value;
+            newStory = newStory.replace(':pluralNoun2:', pluralNoun2);
+        }
+
+        if(customFood.value !== '') {
+            const food = customFood.value;
+            newStory = newStory.replace(':food:', food);
+        }
+
+        if(customEmotion.value !== '') {
+            const emotion = customEmotion.value;
+            newStory = newStory.replace(':emotion:', emotion);
+        }
+
+        if(customVerb.value !== '') {
+            const verb = customVerb.value;
+            newStory = newStory.replace(':verb:', verb);
+        }
+
+        if(customAdjective2.value !== '') {
+            const adjective2 = customAdjective2.value;
+            newStory = newStory.replace(':adjective2:', adjective2);
+        }
 
         if(document.getElementById("uk").checked) {
         const weight = Math.round(300 * 0.0714286) + ' stone';
@@ -44,4 +93,3 @@ randomize.addEventListener('click', result);
         story.style.visibility = 'visible';
     };
 
-    // There is a house on my :noun: that is :adverb: haunted. It's the old :properNounLastname: place that's been adjective for hundreds of years. I can tell the house is :adjective: because there are :pluralNoun: and :adjective: :pluralNoun: outside, and it smells like old :nounFood:. I heard that a kid named 
